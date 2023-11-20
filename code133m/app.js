5e6fedf320fc2c702108dc1667d26df387cf6e96
echoccsf <84433112+echoccsf@users.noreply.github.com>
2023-11-19 23:33:20 -0800

    Update app.js

--- code133m/app.js
+++ code133m/app.js
@@ -1,34 +1,8 @@
-// const registerServiceWorker = async () => {
-//     if ("serviceWorker" in navigator) {
-//         try {
-//             const registration = await 
-//             navigator.serviceWorker.register("serviceworker.js")
-//             if (registration.installing) {
-//                 console.log("Service worker installing");
-//             } else if (registration.waiting) {
-//                 console.log("Service worker installed");
-//             } else if (registration.active) {
-//                 console.log("Service worker active");
-//             }
-//         } catch (error) {
-//             console.error("Service worker failed to register");
-//         }
-//     }
-// };
-
-// registerServiceWorker();
-
 if('serviceWorker' in navigator) {
     navigator.serviceWorker.register('serviceworker.js')
         .then((reg) => console.log('Service worker registered', reg))
         .catch((err) => console.log('Service worker not registered', err));
 }
-
-// function changeBackground(background) {
-//     var element = document.getElementById("url");
-//     element.value = background;
-//     element.innerHTML = background;
-// }
 
 function lightBlue() {
     document.body.style.backgroundImage = "url('lightblue.jpg')";
@@ -41,15 +15,3 @@ function lightGold() {
 function resetBackground() {
     document.body.style.backgroundImage = "none";
 }
-
-// function changeBackground(blue) {
-//     var element = document.getElementById("url");
-//     element.value = background;
-//     element.innerHTML = background;
-// }
-
-// function changeBackground(gold) {
-//     var element = document.getElementById("url");
-//     element.value = background;
-//     element.innerHTML = background;
-// }
