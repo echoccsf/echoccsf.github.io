6d32ba15ff7b95561a3037689369891469d2186d
echoccsf <84433112+echoccsf@users.noreply.github.com>
2023-12-11 18:07:38 -0800

    Update app.js

--- code133m/app.js
+++ code133m/app.js
@@ -1,5 +1,5 @@
 if('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('serviceworker.js')
+    navigator.serviceWorker.register('serviceworker-hw4.js')
         .then((reg) => console.log('Service worker registered', reg))
         .catch((err) => console.log('Service worker not registered', err));
 }
