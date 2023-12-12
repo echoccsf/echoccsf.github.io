a3d95c662219d546c0621c61304628c55e000ec5
echoccsf <84433112+echoccsf@users.noreply.github.com>
2023-12-11 17:59:21 -0800

    Add files for short task
    Short task shown as hw5 in file names

---
+++ code133m/serviceworkerhw5.js
@@ -0,0 +1,42 @@
+const addResourcesToCache = async (resources) => {
+    const cache = await caches.open("v1");
+    await cache.addAll(resources);
+};
+
+self.addEventListener("install", event => {
+    event.waitUntil(
+        addResourcesToCache([
+            "indexhw5.html",
+            "stylehw5.css"
+        ]),
+    );
+});
+
+self.addEventListener("activate", event => {
+    console.log("Service worker activated");
+});
+
+const putInCache = async (request, response) => {
+    const cache = await caches.open("v1");
+
+    if (request.method === 'POST') {
+        console.log('Cannot cache POST requests')
+        return
+    }
+
+    await cache.put(request, response);
+};
+
+const cacheFirst = async (request) => {
+    const responseFromCache = await caches.match(request);
+    if (responseFromCache) {
+        return responseFromCache;
+    }
+    const responseFromNetwork = await fetch(request);
+    putInCache(request, responseFromNetwork.clone())
+    return responseFromNetwork;
+};
+
+self.addEventListener("fetch", (event) => {
+    event.respondWith(caches.match(event.request));
+});
